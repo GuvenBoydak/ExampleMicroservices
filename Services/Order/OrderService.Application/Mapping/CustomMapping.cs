b5e22bc79dc98bfa68bcbe58437eb2e49045e839
Guvn Boydak <gvn.boydak@gmail.com>
2023-04-10 21:29:39 +0300

    add dbContext , mapper, mediatr

---
+++ Services/Order/OrderService.Application/Mapping/CustomMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using OrderService.Application.Dtos;
+using OrderService.Domain.OrderAggregate;
+
+namespace OrderService.Application.Mapping;
+
+public class CustomMapping : Profile
+{
+    public CustomMapping()
+    {
+        CreateMap<Order, OrderDto>().ReverseMap();
+        CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+        CreateMap<Address, AddressDto>().ReverseMap();
+    }
+}
---
+++ Services/Order/OrderService.Infrastructure/DBContext/OrderDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Domain.OrderAggregate;
+
+namespace OrderService.Infrastructure.DBContext;
+
+public class OrderDbContext : DbContext
+{
+    public const string DefaultSchema = "ordering";
+
+    public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Order>().ToTable("Orders", DefaultSchema);
+        modelBuilder.Entity<OrderItem>().ToTable("OrderItems", DefaultSchema);
+
+        modelBuilder.Entity<OrderItem>().Property(x => x.Price).HasColumnType("decimal(18,2)");
+
+        modelBuilder.Entity<Order>().OwnsOne(o => o.Address).WithOwner();
+        
+
+
+    }
+}
